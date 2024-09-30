969e00c6887b2c28c5506ba5c0a668347f6fd625
Jin Lee <noctua@umich.edu>
2024-09-30 00:15:12 -0400

    crosstalk_main.py skeleton added.

--- crosstalk_main.py
+++ crosstalk_main.py
@@ -13,3 +13,23 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import resample
+
+# Helper Functions ================================================= #
+def generate_feature_vector(df: pd.DataFrame) -> dict[str, float]:
+    return None
+
+def impute_missing_values(X: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    return None
+
+def normalize_feature_matrix(X: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    return None
+
+# main() =========================================================== #
+
+def main() -> None:
+    # Driver
+
+    return None
+
+if __name__ == "__main__":
+    main()
