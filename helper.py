7c9ffb00c8dd5c67e804d0adf8f9420d008dd9a4
Jin Lee <noctua@umich.edu>
2024-10-01 10:59:51 -0400

    helper.py modified. Working on feature extraction.

--- helper.py
+++ helper.py
@@ -29,18 +29,21 @@ def get_train_test_split_small() -> tuple[npt.NDArray[np.float64], npt.NDArray[n
     Returns the features and labels for train and test sets, followed by the names of features.
     """
     path =''
-    df_cor_n = pd.read_csv(path+'data/correlation_n.csv')
+    df_cor_n = pd.read_csv(path+'data/correlation_n.csv') # Read negative crosstalks
+    df_cor_p = pd.read_csv(path+'data/correlation_p.csv') # Read positive crosstalks
     df_cor_n = df_cor_n[['ac_loc','pho_loc', 'cor']] # Select desired columns
-    df_cor_n = df_cor_n[:2000]
+    df_cor_p = df_cor_p[['ac_loc','pho_loc', 'cor']] # Select desired columns
+    df_cor = pd.concat([df_cor_n, df_cor_p], axis=0)
 
     crosstalks = {}
-    for i in tqdm(range(2000), desc='Loading files from disk'):
-        crosstalks[i] = df_cor_n[i:i+1]
+    for i in tqdm(range(df_cor.shape[0]), desc='Loading files from disk'):
+        crosstalks[i] = df_cor[i:i+1]
     
     features = Parallel(n_jobs=16)(delayed(crosstalk_main.generate_feature_vector)(df) for _, df in tqdm(crosstalks.items(), desc='Generating feature vectors'))
     df_features = pd.DataFrame(features).sort_index(axis=1)
     feature_names = df_features.columns.tolist()
-    X, y = df_features.values, df_cor_n['cor'].values
+    X, y = df_features.to_numpy(), df_features['cor'].to_numpy()
+    y = crosstalk_main.binarize_values(y)
     X = crosstalk_main.impute_missing_values(X)
     X = crosstalk_main.normalize_feature_matrix(X)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, stratify=y, random_state=3)
